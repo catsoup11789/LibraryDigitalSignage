fe2df9c537fb6b0da85fb350b9a6c91071dc471c
Kirstien Kroeger <catsoup11789@users.noreply.github.com>
2020-04-27 15:42:07 -0500

    Update header.php

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,10 +8,10 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta http-equiv="refresh" content="1800; URL='<?= $url ?>'">
 
-    <title>Salina Public Library</title>
+    <title><?= site()->title() ?></title>
     <link rel="stylesheet" id="ebor-google-font-css" href="//fonts.googleapis.com/css?family=Open+Sans%3A200%2C300%2C400%2C400i%2C500%2C600%2C700%7CLora%3A400%2C400i%2C700%2C700i%7CMaterial+Icons&amp;ver=10.5.15" type="text/css" media="all">
     <?= css('assets/css/index.css') ?>
-    <link rel="stylesheet" type="text/css" href="http://ds.salinalibrary.info/assets/css/templates/default.css?<?= date('his'); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?= site()->url() ?>/assets/css/templates/default.css?<?= date('his'); ?>" />
 
 
 </head>
