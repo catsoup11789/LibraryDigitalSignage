0c2cf1c37713e55dab41c0a82e47c04852d5882f
Kirstien Kroeger <catsoup11789@users.noreply.github.com>
2020-04-27 15:40:29 -0500

    Update gallery.php

--- site/snippets/gallery.php
+++ site/snippets/gallery.php
@@ -33,7 +33,7 @@ if(strpos($url, 'portrait')) {
 <?php foreach ($gallery->sortBy('sort','asc') as $image): ?>
 
 <!-- filter for only images that are supposed to be visible -->
-<?php if (($image->expire()->toDate('Y-m-d') > date('Y-m-d')) /*&& ($image->start()->toDate('Y-m-d') <= date('Y-m-d'))*/): ?>
+<?php if (($image->expire()->toDate('Y-m-d') > date('Y-m-d')) && ($image->start()->toDate('Y-m-d') <= date('Y-m-d'))): ?>
 
 <!-- match orientation to image -->
 <?php if($image->orientation() == $orientation): ?>
@@ -41,7 +41,7 @@ if(strpos($url, 'portrait')) {
 
     <!-- if OPAC, show register button, but only if URL AND button text is provided -->
     <?php if((strpos($url, 'opac')) && ($image->link()->isNotEmpty()) && ($image->text()->isNotEmpty())): ?>
-    <a href="http://ds.salinalibrary.info/frame?shortcut=<?= $image->link() ?>" class="btn-register"><?= $image->text() ?></a>
+    <a href="<?php $site->url(); ?>/frame?shortcut=<?= $image->link() ?>" class="btn-register"><?= $image->text() ?></a>
     <?php endif ?>
 
 </div>
